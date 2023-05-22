72ebccc01031393c8167d0b4ee81debd1aad8c4c
LiquicChris <cshokunb@uci.edu>
2023-05-22 11:37:23 -0700

    Initial Commit

---
+++ Sleep-clean/Sleep_cleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_cleanApp.swift
+//  Sleep-clean
+//
+//  Created by slmrc on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Sleep_cleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
